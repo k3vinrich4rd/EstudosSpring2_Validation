0deabce4a3c52194b819c1e2094fd49d711c955c
k3vinrich4rd <kevinrichardve@gmail.com>
2022-09-12 11:55:02 -0300

    Inserindo implementação de escopo no projeto

--- src/main/java/com/example/trabalhandoComValidation/controller/ClienteController.java
+++ src/main/java/com/example/trabalhandoComValidation/controller/ClienteController.java
@@ -7,15 +7,11 @@ import com.example.trabalhandoComValidation.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
